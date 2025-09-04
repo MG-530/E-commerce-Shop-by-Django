2b0d926c13c5cb717655de374496d5619b7ac27e
MG-530 <mg1383530@gmail.com>
2025-09-04 16:12:18 +0330

    add vendor views

--- code/vendor/views.py
+++ code/vendor/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Vendor, VendorProduct, VendorReview
+from .serializers import VendorSerializer, VendorProductSerializer, VendorReviewSerializer
 
-# Create your views here.
+class VendorViewSet(viewsets.ReadOnlyModelViewSet):
+    # ViewSet for vendors. Read-only for public access.
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+class VendorProductViewSet(viewsets.ModelViewSet):
+    # ViewSet for vendor products.
+    queryset = VendorProduct.objects.all()
+    serializer_class = VendorProductSerializer
+
+class VendorReviewViewSet(viewsets.ModelViewSet):
+    # ViewSet for vendor reviews.
+    queryset = VendorReview.objects.all()
+    serializer_class = VendorReviewSerializer
