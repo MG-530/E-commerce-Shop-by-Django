9530c0ab359e8853e0791821e11e8e30ec976627
MG-530 <mg1383530@gmail.com>
2025-09-05 11:19:02 +0330

    fix(users): Implement custom auth token

--- code/users/urls.py
+++ code/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
-from .views import UserViewSet, AddressViewSet, LoginView
+from .views import UserViewSet, AddressViewSet, CustomAuthToken
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -9,5 +9,4 @@ router.register(r'addresses', AddressViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-token-auth/', LoginView.as_view()),
-]
+    path('api-token-auth/', CustomAuthToken.as_view(), name='api_token_auth'),]
--- code/users/views.py
+++ code/users/views.py
@@ -3,6 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from .models import User, Address
 from .serializers import UserSerializer, AddressSerializer, AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import status
 
 class UserViewSet(viewsets.ModelViewSet):
     # ViewSet for handling user data.
@@ -23,9 +27,14 @@ class AddressViewSet(viewsets.ModelViewSet):
         # Automatically associate new addresses with the current user.
         serializer.save(user=self.request.user)
 
-class LoginView(ObtainAuthToken):
-    """
-    A custom view to handle user authentication and return an auth token.
-    Uses the custom AuthTokenSerializer to validate login with email instead of username.
-    """
-    serializer_class = AuthTokenSerializer
+class CustomAuthToken(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.id,
+            'email': user.email
+        }, status=status.HTTP_200_OK)
